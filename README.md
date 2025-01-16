75fcbf13fcde61511326424854d3e5696933157d
Edson Domingos <edomingoswork@gmail.com>
2025-01-16 00:44:24 -0300

    versao 1.0 da tela de login

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# tela-login-basica
+# Tela de Login
+
+> Projeto feito apenas com HTML, CSS e Javascript
+
+## Para a execução deste projeto há basicamente 2 opções
+
+1. Baixar este repositorio e executar o arquivo index.html em qualquer navegador (Chrome, Firefox, Edge ...)
+
+2. Acessa o link abaixo para testar a tela de login disponivel online no github pages:
+[Acessar Tela de Login](https://github.com/Edsondomingos/Desafio-de-codigo-php-tecnotech)
---
+++ script.js
@@ -0,0 +1,87 @@
+const form = document.querySelector('form')
+const fieldset = document.querySelector('fieldset')
+const inputs = document.querySelectorAll('input')
+const section = document.querySelector('section')
+const email = document.querySelector('[data-email]')
+const password = document.querySelector('[data-password]')
+const aviso = document.querySelector('[data-aviso]')
+
+
+// function para autenticar usuario
+async function autenticar() {
+    try {
+        await fetch('dados.json')
+            .then(response => response.json().then(dados => {
+
+                dados.forEach(element => {
+                    if (element.email == email.value && element.password == password.value) {
+                        form.style = `
+                        transform: scale(1.5);
+                        border-radius: 100%;
+                        transition: 2s;
+                        width: 250px;
+                `
+                        setTimeout(() => {
+                            form.style.display = 'none'
+                            section.style.display = 'block'
+                        }, 3000)
+                        aviso.style.display = 'none'
+                        return
+                    }
+
+                    aviso.textContent = 'email ou senha incorreto(s)'
+                    aviso.style.display = 'block'
+
+                });
+            }))
+            .catch(e => aviso.textContent = 'Sistema Indisponivel')
+    } catch (erro) {
+        aviso.textContent = 'Tente novamente mais tarde!'
+    }
+}
+
+// Verifica email e senha e mostra avisos correspondentes. Se sucesso logada no sistema
+form.addEventListener('submit', async e => {
+    e.preventDefault()
+
+    if (email.value && password.value && password.value.length >= 6) {
+        await autenticar(e)
+    } else {
+        aviso.textContent = 'Digite um email e/ou senha'
+        aviso.style.display = 'block'
+    }
+    setTimeout(() => aviso.style.display = 'none', 4000)
+    password.chec
+})
+
+// Aparencia e aviso sobre requisitos de senha
+password.addEventListener('keyup', () => {
+    if (password.value.length < 6) {
+        password.style.outline = '3px solid red'
+        aviso.textContent = 'Minimo 6 caracteres'
+        aviso.style.display = 'block'
+    } else {
+        password.style.outline = '3px solid rgb(55,240,40)'
+        aviso.style.display = 'none'
+    }
+})
+
+// Animação ao estar com o mouse sobre o formulario
+fieldset.addEventListener('mouseover', (e) => {
+    inputs.forEach(e => e.style.visibility = 'visible')
+    document.querySelector('legend').style = `
+    font-size: 22px;
+    transform: translateY(0) translateX(0);
+    transition: 1s;`
+    document.querySelector('button').style.visibility = 'visible'
+
+})
+
+fieldset.addEventListener('mouseleave', (e) => {
+    inputs.forEach(e => e.style.visibility = 'hidden')
+    document.querySelector('legend').style = `
+    font-size: 50px;
+    transform: translateY(100) translateX(190);
+    transition: 1s;`
+    document.querySelector('button').style.visibility = 'hidden'
+})
